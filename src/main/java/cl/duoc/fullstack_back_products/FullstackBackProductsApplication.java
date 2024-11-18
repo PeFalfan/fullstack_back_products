f4c48a9b98b2286ef40026f996e8dbafc9882747
Pedro Falfan <pe.falfan@duocuc.cl>
2024-11-18 15:55:04 -0300

    first commit

---
+++ src/main/java/cl/duoc/fullstack_back_products/FullstackBackProductsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.fullstack_back_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/fullstack_back_products/FullstackBackProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.fullstack_back_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
